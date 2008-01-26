4cc23ce7afad3c84c75285c94e959baa440688fe
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-26 10:25:06 +0000

    lang/perl/HTML-Entities-ConvertPictogramMobileJp: Checking in changes prior to tagging of version 0.04.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 5991)
    +++ Changes	(local)
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTML::Entities::ConvertPictogramMobileJp
     
    +0.04
    +
    +    - fixed build dependencies(YAML).
    +
     0.03
     
         - support pair of the pictograms.
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTML-Entities-ConvertPictogramMobileJp/trunk@5561 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTML/Entities/ConvertPictogramMobileJp.pm
+++ lib/HTML/Entities/ConvertPictogramMobileJp.pm
@@ -1,7 +1,7 @@
 package HTML::Entities::ConvertPictogramMobileJp;
 use strict;
 use warnings;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use Encode;
 use Encode::JP::Mobile;
 use Encode::JP::Mobile::Character;
