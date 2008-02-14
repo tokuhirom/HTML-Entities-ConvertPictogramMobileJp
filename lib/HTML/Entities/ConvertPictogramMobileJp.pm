8c98716118e0a9aa12b1110ac04b8b8b7f453637
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-02-14 01:52:57 +0000

    lang/perl/HTML-Entities-ConvertPictogramMobileJp: Checking in changes prior to tagging of version 0.06.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 7883)
    +++ Changes	(local)
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTML::Entities::ConvertPictogramMobileJp
     
    +0.06
    +
    +    - enhancement docs and tests
    +
     0.05
     
         - do not use Encode::JP::Mobile's svn HEAD.
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTML-Entities-ConvertPictogramMobileJp/trunk@6668 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTML/Entities/ConvertPictogramMobileJp.pm
+++ lib/HTML/Entities/ConvertPictogramMobileJp.pm
@@ -1,7 +1,7 @@
 package HTML::Entities::ConvertPictogramMobileJp;
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use Encode;
 use Encode::JP::Mobile;
 use Params::Validate;
