1db35767e72efa96d402caab5d93aafcde01d7b4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-13 11:51:04 +0900

    Checking in changes prior to tagging of version 0.09.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1744960..9c3466e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTML::Entities::ConvertPictogramMobileJp
    
    +0.09
    +
    +    - modernize package(no feature changes)
    +
     0.08
    
         - skip some tests when your H::MA is too old

--- lib/HTML/Entities/ConvertPictogramMobileJp.pm
+++ lib/HTML/Entities/ConvertPictogramMobileJp.pm
@@ -1,7 +1,7 @@
 package HTML::Entities::ConvertPictogramMobileJp;
 use strict;
 use warnings;
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 use Encode;
 use Encode::JP::Mobile;
 use Params::Validate;
